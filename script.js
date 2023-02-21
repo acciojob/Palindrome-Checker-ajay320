052e958210aae32ee2d7c6860eb9338205e33b4f
Ajay Kumar <69907987+ajay320@users.noreply.github.com>
2023-02-21 21:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+let a=str.trim().toLowerCase().length;
+	
+	 // const len = str.length;
 
+    
+    for (let i = 0; i < a / 2; i++) {
+
+       
+        if (str[i] !== str[a - 1 - i]) {
+            return true;
+        }
+    }
+    return false;
 }
 module.exports = palindrome
